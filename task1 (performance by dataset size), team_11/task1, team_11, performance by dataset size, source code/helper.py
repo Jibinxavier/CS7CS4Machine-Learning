c073c8488872c17fd428069febb6c474bace3878
Jibin Xavier <xavierj@tcd.ie>
2017-10-26 20:24:34 +0100

    regression is working but not logistic regression

--- task1 (performance by dataset size), team_11/task1, team_11, performance by dataset size, source code/helper.py
+++ task1 (performance by dataset size), team_11/task1, team_11, performance by dataset size, source code/helper.py
@@ -33,17 +33,47 @@ def get_data(urls):
             unzip_data("temp_file.zip", data_folder_path)
         os.remove("temp_file.zip")
 
-
-
  
+
 def get_news_dataset():
     zip_f = "data/OnlineNewsPopularity.zip"
     data_folder_path ="data"
     dataset_path = "data/OnlineNewsPopularity/OnlineNewsPopularity.csv"
-    unzip_data(zip_f,data_folder_path)
+    if not os.path.exists(dataset_path):
+        unzip_data(zip_f,data_folder_path)
+        os.remove(zip_f)
     
     df = pd.read_csv(dataset_path)
     return df
+
+def create_classes(row):
+    """
+
+    """
+
+  
+
+    if(row[' shares'] <= 140550):
+        
+        return 0
+    elif( row[' shares'] > 140550 and row[' shares'] <= 281101  ):
+        print("hewr")
+        return 1
+    elif( row[' shares'] > 281101 and row[' shares'] <= 421651  ):
+         
+        return 2
+    elif( row[' shares'] > 421651 and row[' shares'] <= 562202  ):
+         
+        return 3
+
+    elif( row[' shares'] > 562202 and row[' shares'] <= 702752  ):
+         
+        return 4
+    else:
+        
+        return 5
+
+
  
 # wget.download("https://www.dropbox.com/sh/euppz607r6gsen2/AABABUTdx7YqCeBquA1Ky7z8a/The%20SUM%20dataset?dl=1#")
 
